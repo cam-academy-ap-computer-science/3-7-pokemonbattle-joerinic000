a80d744624cf7e90fcfd2e9d345eb51f60614bb3
joerinic000 <joerinic000@CAM-LAB-11.bgsd.k12.wa.us>
2019-11-01 07:49:44 -0700

    Still working

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,25 @@
-
+import java.util.*;
 public class PokemonBattle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			battleStart();
+			damage();
+			statsTable();
+	}
+	public static void battleStart() {
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println("Which Pokémon do you choose?");
+		String pokemon = userInput.nextLine();
+		System.out.println("You chose " + pokemon);
+		System.out.println("They chose Zebstrika.");
 
 	}
-
+	public static void damage() {
+		
+	}
+	public static void statsTable() {
+		
+	}
 }
