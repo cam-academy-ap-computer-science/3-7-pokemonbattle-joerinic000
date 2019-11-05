64a979d9b773b404c77024e4e309877d6cf977c1
joerinic000 <joerinic000@CAM-LAB-11.bgsd.k12.wa.us>
2019-11-05 07:35:01 -0800

    Not complete.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -21,10 +21,19 @@ public class PokemonBattle {
 		Scanner userInput = new Scanner (System.in);
 		System.out.println("Trainer, what is your " + pokemon + "'s level?");
 		int level = userInput.nextInt();
-		
-		
+		System.out.println("Trainer, what is your " + pokemon + "'s attack?");
+		int attack = userInput.nextInt();
+		System.out.println("Trainer, what is your " + pokemon + "'s defense?");
+		int defence = userInput.nextInt();
+		int hP = userInput.nextInt();
+		int damage = calc(level, attack, defence);
 	}
 	public static void statsTable() {
 		
 	}
+	public static int calc (int level, int attack, int defence) {
+		int damage = (((2 * level + 10)/250) + (attack / defence) * 2 + 2);
+		return damage;
+	}
 }
+
